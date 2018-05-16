3805b029bbc61845b3b0a97aaa689665b2b8d103
carlos.kuretzki <carlos.kuretzki@LUME137.lume.intra>
2018-05-16 14:40:57 -0300

    first commit

---
+++ AbstrataFormas/src/Hexagon.java
@@ -0,0 +1,8 @@
+
+public class Hexagon extends TwoDShape{
+
+	double area() {
+		return 0;
+	}
+
+}
---
+++ AbstrataFormas/src/Triangle.java
@@ -0,0 +1,33 @@
+public class Triangle extends TwoDShape{
+
+	private String style;
+	
+	Triangle() {
+		super();
+		style = "none";
+	}
+	
+	Triangle(String s, double w, double h) {
+		super(w, h, "triangle");
+		style = s;
+	}
+	
+	Triangle(double x) {
+		super(x, "triangle");
+		style = "filled";
+	}
+	
+	Triangle(Triangle ob) {
+		super(ob);
+		style = ob.style;
+	}
+	
+	@Override
+	double area() {
+		return getWidth() * getHeight() / 2;
+	}
+	
+	void showStyle() {
+		System.out.println("Triangulo é ");
+	}
+}
