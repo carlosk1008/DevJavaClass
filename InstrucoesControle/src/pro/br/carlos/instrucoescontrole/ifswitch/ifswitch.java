65140d7bdb04425c2808c51fa1032ac9ec3c179f
carlos.kuretzki <carlos.kuretzki@LUME137.lume.intra>
2018-05-16 14:37:12 -0300

    first commit

---
+++ InstrucoesControle/src/pro/br/carlos/instrucoescontrole/ifswitch/ifswitch.java
@@ -0,0 +1,31 @@
+package pro.br.carlos.instrucoescontrole.ifswitch;
+
+public class ifswitch {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 4; i++) {
+			
+			switch(i) {
+				case 0:
+					System.out.println("zero");
+					break;
+				case 1:
+					System.out.println("um");
+					break;
+				case 2:
+					System.out.println("dois");
+					break;
+				case 3:
+					System.out.println("três");
+					break;
+				default:
+					System.out.println("três");					
+
+			}
+			
+		}	
+		
+	}
+
+}
