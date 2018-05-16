ce1b6ee200b0380538eb8b26c8828442aa951e0d
carlos.kuretzki <carlos.kuretzki@LUME137.lume.intra>
2018-05-16 14:39:31 -0300

    first commit

---
+++ GetterSetter/src/Carro.java
@@ -0,0 +1,20 @@
+
+public class Carro {
+
+	private double valor;
+	private String modelo;
+	private String marca;
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+}
---
+++ GetterSetter/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Carro c = new Carro();
+		
+		c.setModelo("Cruze");
+		c.setValor(96790.00);
+		
+		System.out.println(c.getModelo());
+		System.out.println(c.getValor());
+
+	}
+
+}
